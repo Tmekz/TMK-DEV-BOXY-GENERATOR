fa586246499566408d528ab8768375701d4443a4
Tarig Mekhazni <tmk-dev@hotmail.com>
2024-10-16 11:56:57 +0200

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,18 @@
+import logo from "/TMK-LOGO-BLACK.svg";
+
 const Header = () => {
   return (
-    <div className="py-4 border-b-2 border-gray-200">
-      <h1 className="font-semibold text-2xl text-center">BOXY GENERATOR</h1>
+    <div className="py-6 border-b-2 border-gray-200">
+      <img
+        className="w-38 h-10 absolute left-6 sm:flex hidden"
+        src={logo}
+        alt="logo"
+      />
+      <h1 className="font-semibold text-2xl text-center mx-auto">
+        BOXY GENERATOR
+      </h1>
     </div>
+    
   );
 };
 
